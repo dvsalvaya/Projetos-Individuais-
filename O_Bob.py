aee53978f9941e8487a1286ccfa637549287df10
Davi Salvaya <dvsalvaya@gmail.com>
2024-06-07 19:56:50 -0300

    O_Bob 2.0

--- O_Bob.py
+++ O_Bob.py
@@ -2,8 +2,7 @@ import speech_recognition as sr
 import pyttsx3
 import datetime
 import wikipedia
-import pywhatkit
-from tkinter import *
+import pywhatkit as pt
 import sys
 
 
@@ -13,61 +12,68 @@ maquina = pyttsx3.init()
 def executa_comando():
  try:
     with sr.Microphone() as source:
-        maquina.say('Eu sou o Bob, como posso ajudar?')
-        maquina.runAndWait()
+        
         print('Ouvindo...')
         voz = audio.listen(source)
         comando = audio.recognize_google(voz, language='pt-BR')
         comando =comando.lower()
-        if "Bob" in comando:
+        if "bob" in comando:
          comando = comando.replace('Bob','')
-         maquina.say(comando)
-         maquina.runAndWait()
+         
  except:
   print("Microfone não está ok")
-
+  inicio()
+ 
  return comando
 
 def comando_voz_usuario():
  comando = executa_comando()
- if "horas" in comando:
+ if "bob" in comando:
+  if "horas" in comando:
     hora = datetime.datetime.now().strftime('%H:%M')
     maquina.say('Agora são' + hora)
     maquina.runAndWait()
- elif "procure por" in comando:
+    maquina.say('Mais alguma coisa?')
+    maquina.runAndWait()
+  elif "procure por" in comando:
      procurar = comando.replace('procure por','')
      wikipedia.set_lang("pt")
      resultado = wikipedia.summary(procurar, 2)
      print(resultado)
      maquina.say(resultado)
      maquina.runAndWait()
- elif "toque" in comando:
+     maquina.say('Mais alguma coisa?')
+     maquina.runAndWait()
+  elif "toque" in comando:
      musica = comando.replace('toque', '')
-     resultado = pywhatkit.playonyt(musica)
+     resultado = pt.playonyt(musica)
      maquina.say("Tocando música")
      maquina.runAndWait()
- elif "desligar" in comando:
+  elif "desligar" in comando:
    sys.exit()
- 
+  elif "mensagem" in comando:
+   #Nome da pessoa e o número
+   if '' in comando :
+     phone_number = '+55'
+     message = comando.replace('bob mensagem ','')
+   elif '' in comando:
+     phone_number = '+55'
+     message = comando.replace('bob mensagem ','')  
+   elif '' in comando:
+     phone_number = '+55'
+     message = comando.replace('bob mensagem ','')
+   pt.sendwhatmsg_instantly(phone_number,message,7,True,2)
+   maquina.say('Mais alguma coisa?')
+   maquina.runAndWait()
+    
+
   
 
+def inicio():
+ while True:
+  comando_voz_usuario()
+  
 
-while True:
- comando_voz_usuario()
-
-
-#bob = Tk()
-#bob.geometry('400x300')
-#bob.title('Eu sou o Bob')
-#bob.config(bg='#c7bf56')
-
-
-#lbl = Label (bob, text='Oi, eu sou o Bob', foreground='yellow', bg='black' )(font = ('orca role, 20'))
-#lbl.grid(column=0, row=0, padx=20 , pady=60,)
-
-
-#b=Button(bob, text='Aperte para eu poder te ouvir :)' , command= comando_voz_usuario,
- #        foreground='darkblue', bg='lightblue')
-#b.grid(column=0, row=1, padx=20, pady=10)
-
-#bob.mainloop()
+maquina.say('Eu sou o Bob, como posso ajudar?')
+maquina.runAndWait()
+inicio()
